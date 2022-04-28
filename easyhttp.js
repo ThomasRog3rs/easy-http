dc11a0462d5941314aedf0683db39ab6856b3d48
Thomas Rogers <42161850+ThomasRog3rs@users.noreply.github.com>
2022-04-28 13:57:00 +0100

    format the data in the correct content type
    Changes to format data based on contentType

--- easyhttp.js
+++ easyhttp.js
@@ -9,52 +9,79 @@
  *
  **/
 
-class EasyHTTP {
-  // Make an HTTP GET Request
-  async get(url) {
-    const response = await fetch(url);
-    const resData = await response.json();
-    return resData;
-  }
+class HttpAjax {
+	//This function is to format the data in the correct content type
+	async formatData(contentType, data){
+		let formattedData;
+		switch(contentType){
+			case "application/json":
+				formattedData = await data.json();
+				break;
+			case "text/xml":
+				formattedData = await data.text();
+				let parser = new DOMParser();
+				formattedData = parser.parseFromString(formattedData, contentType);
+				break;
+			case "text/plain":
+				formattedData = await data.text();
+				break;
+			default:
+		}
+		
+		return formattedData;
+	}
+	
+  	// Make an HTTP GET Request
+  	async get(url, contentType = "application/json") {
+    	const response = await fetch(url, {
+      		method: 'GET',
+      		headers: {
+        		'Content-type': contentType
+      		}
+    	});
+    	
+    	const formatResponse = this.formatData(contentType, response);
+    	return formatResponse;
+  	}
 
-  // Make an HTTP POST Request
-  async post(url, data) {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    });
+  	// Make an HTTP POST Request
+  	async post(url, data, contentType = "application/json") {
+    	const response = await fetch(url, {
+      		method: 'POST',
+      		headers: {
+        		'Content-type': contentType
+      		},
+      		body: JSON.stringify(data)
+    	});
 
-    const resData = await response.json();
-    return resData;
-  }
+    	const formatResponse = this.formatData(contentType, response);
+    	return formatResponse;
+  	}
 
-  // Make an HTTP PUT Request
-  async put(url, data) {
-    const response = await fetch(url, {
-      method: 'PUT',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    });
+    // Make an HTTP PUT Request
+  	async put(url, data, contentType = "application/json") {
+	    const response = await fetch(url, {
+	      method: 'PUT',
+	      headers: {
+	        'Content-type': contentType
+	      },
+	      body: JSON.stringify(data)
+	    });
+	
+	    const formatResponse = this.formatData(contentType, response);
+	    return formatResponse;
+	}
 
-    const resData = await response.json();
-    return resData;
-  }
+   // Make an HTTP DELETE Request
+   async delete(url, contentType = "application/json") {
+      const response = await fetch(url, {
+	      method: 'DELETE',
+	      headers: {
+	        'Content-type': contentType
+	      }
+	  });
 
-  // Make an HTTP DELETE Request
-  async delete(url) {
-    const response = await fetch(url, {
-      method: 'DELETE',
-      headers: {
-        'Content-type': 'application/json'
-      }
-    });
-
-    const resData = await 'Resource Deleted...';
-    return resData;
-  }
+	  const formatResponse = this.formatData(contentType, response);
+	  return formatResponse;
+   }
 }
